b9bf49697233dac19ad6d95363790a910dc6b95a
Christian Hemefor <hemeforc@gmail.com>
2022-06-18 10:35:12 -0600

    Assignment #1

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -8,8 +10,9 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.use('/campsites', campsiteRouter)
-
+app.use('/campsites',campsiteRouter);
+app.use('/promotions',promotionRouter);
+app.use('/partners', partnerRouter);
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res) => {
@@ -21,3 +24,5 @@ app.use((req, res) => {
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
